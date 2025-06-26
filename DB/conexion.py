b66f53ea622f8d8ce6d8d02d79351dd523000827
Mario_Brnal10 <145825327+MarioBrnal10@users.noreply.github.com>
2025-06-25 23:58:42 -0600

    Preparacion
    se prepararon las carpetas para tener mejor orden en el proyecto y se avanzo hasta la creación del archivo conexión para la base de datos.

---
+++ DB/conexion.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# 🚨 Rellena tus datos aquí
+DB_USER = 'root'
+DB_PASSWORD = ''
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_NAME = 'lana4'
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Crear el motor de conexión
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Crear la sesión
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base para modelos
+Base = declarative_base()
