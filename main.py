8ab5ced1a659f7e44db0d26e9254e938da746ecf
Mario_Brnal10 <145825327+MarioBrnal10@users.noreply.github.com>
2025-08-11 00:22:38 -0600

    2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routers.transacciones import routerTransacciones
 from routers.presupuestos import routerPresupuestos
 from routers.grafica import routerGrafica
 from routers.PagosFijos import routerPagosFijos
+from routers.categorias import routercategorias
 app = FastAPI(
     title='API LANA APP',
     description='API PARA LA APLICACION LANA APP',
@@ -24,6 +25,8 @@ app.add_middleware(
 app.include_router(routerUsuarios)
 app.include_router(routerTransacciones)
 app.include_router(routerPresupuestos)
+
+app.include_router(routercategorias)
 app.include_router(routerGrafica)
 app.include_router(routerPagosFijos)
 
